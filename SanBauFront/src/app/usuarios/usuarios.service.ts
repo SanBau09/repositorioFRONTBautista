e83a7e9c680b19c265940385a607106f4492e19e
Sandra <sandrabautistacencerrado@gmail.com>
2024-06-14 22:01:58 +0200

    comentado el usuarios.service

--- SanBauFront/src/app/usuarios/usuarios.service.ts
+++ SanBauFront/src/app/usuarios/usuarios.service.ts
@@ -7,6 +7,9 @@ import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 import { Pais } from './pais';
 
+/**
+ * Servicio para la gestión de operaciones relacionadas con usuarios y países.
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -17,6 +20,10 @@ export class UsuariosService {
 
   constructor(private http: HttpClient, private router: Router, private authService: AuthService) { }
 
+    /**
+   * Agrega el token de autorización a las cabeceras HTTP.
+   * returns HttpHeaders con el token de autorización si está autenticado, de lo contrario, retorna las cabeceras estándar.
+   */
   private agregarAuthorizationHeader(){
     let token = this.authService.token;
 
@@ -26,6 +33,11 @@ export class UsuariosService {
     return this.httpHeaders;
   }
 
+  /**
+   * Verifica si la respuesta HTTP indica que el usuario no está autorizado (401) o acceso denegado (403).
+   * param e La respuesta HTTP de error.
+   * returns true si el usuario no está autorizado o tiene acceso denegado, false en otros casos.
+   */
   private isNoAutorizado(e): boolean{
     if(e.status == 401){
       if(this.authService.isAuthenticated()){
@@ -44,6 +56,11 @@ export class UsuariosService {
     return false;
   }
   
+  /**
+   * Realiza una solicitud HTTP POST para registrar un nuevo usuario.
+   * param usuario El objeto Usuario que se va a registrar.
+   * returns Observable que emite el usuario registrado en caso de éxito.
+   */
   create(usuario: Usuario) : Observable<Usuario> {
     return this.http.post(this.urlEndPoint + "/registro", usuario, {headers:this.agregarAuthorizationHeader()}).pipe(
       map( (response : any) => response.usuario as Usuario),
@@ -64,6 +81,11 @@ export class UsuariosService {
     );
   }
 
+  /**
+   * Realiza una solicitud HTTP PUT para actualizar los datos de un usuario existente.
+   * param usuario El objeto Usuario con los datos actualizados.
+   * returns Observable que emite la respuesta de la solicitud HTTP.
+   */
   editarUsuario(usuario: Usuario): Observable<any>{
     return this.http.put<any>(` ${this.urlEndPoint}/${usuario.id}`, usuario, {headers: this.agregarAuthorizationHeader()}).pipe(
       catchError(e=> {
@@ -82,6 +104,10 @@ export class UsuariosService {
     )
   }
 
+  /**
+   * Obtiene la lista de países desde el backend.
+   * returns Observable que emite un array de objetos de tipo Pais.
+   */
   getPaises(): Observable<Pais[]>{
     return this.http.get<Pais[]>(this.urlEndPoint + '/paises', {headers: this.agregarAuthorizationHeader()}).pipe(
       catchError(e=>{
@@ -90,7 +116,11 @@ export class UsuariosService {
     );
   }
 
-  
+  /**
+   * Obtiene un usuario específico por su nombre de usuario desde el backend.
+   * param username El nombre de usuario del usuario que se desea obtener.
+   * returns Observable que emite el objeto Usuario encontrado.
+   */
   getUsuarioPorUsername(username): Observable<Usuario>{
     return this.http.get<Usuario>(` ${this.urlEndPoint}/user/${username}`, {headers: this.agregarAuthorizationHeader()}).pipe(
       catchError(e =>{
@@ -106,6 +136,11 @@ export class UsuariosService {
     }));
   }
 
+    /**
+   * Obtiene un usuario específico por su ID desde el backend.
+   * param id El ID del usuario que se desea obtener.
+   * returns Observable que emite el objeto Usuario encontrado.
+   */
   getUsuario(id): Observable<Usuario>{
     return this.http.get<Usuario>(` ${this.urlEndPoint}/${id}`, {headers: this.agregarAuthorizationHeader()}).pipe(
       catchError(e =>{
