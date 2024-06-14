9ab328aac94cef614980c803a08665d9a1755b74
Sandra <sandrabautistacencerrado@gmail.com>
2024-06-14 22:33:29 +0200

    metodos comentados para el form-registro

--- SanBauFront/src/app/usuarios/form-registro/form-registro.component.ts
+++ SanBauFront/src/app/usuarios/form-registro/form-registro.component.ts
@@ -23,13 +23,19 @@ export class FormRegistroComponent implements OnInit  {
 
   constructor(public authService:AuthService, private usuariosService: UsuariosService, private router: Router, private activatedRoute: ActivatedRoute){}
 
+  /**
+   * Método para cargar los datos del usuario actual si existe
+   * Obtiene y carga la lista de países disponibles
+   */
   ngOnInit(){
     this.cargarUsuario();
     this.usuariosService.getPaises().subscribe( paises => this.paises = paises);
   
   }
 
- 
+  /**
+   * Carga los datos del usuario actual en el formulario de registro si está autenticado.
+   */
   cargarUsuario(): void{
     this.activatedRoute.params.subscribe(params => {
       let usuarioActual = this.authService.usuario;
@@ -39,6 +45,11 @@ export class FormRegistroComponent implements OnInit  {
     })
   }
   
+  /**
+   * Crea un nuevo usuario utilizando el servicio UsuariosService.
+   * Si la operación es exitosa, redirige al usuario a la página de inicio de sesión.
+   * Si falla, muestra un mensaje de error utilizando SweetAlert2.
+   */
   public create(): void{
     this.usuariosService.create(this.usuario).subscribe({
       next:
@@ -53,6 +64,11 @@ export class FormRegistroComponent implements OnInit  {
     });
   }
 
+  /**
+   * Edita los datos del usuario utilizando el servicio UsuariosService.
+   * Si la operación es exitosa, redirige al usuario a la página de la galería.
+   * Si falla, muestra un mensaje de error y almacena los errores recibidos en this.errores.
+   */
   editar(): void{
     this.usuariosService.editarUsuario(this.usuario).subscribe({
         next:
@@ -66,6 +82,13 @@ export class FormRegistroComponent implements OnInit  {
         });
   }
 
+  /**
+   * Compara dos objetos de tipo Pais para determinar si son iguales.
+   * Se utiliza en la interfaz para preseleccionar el país actual del usuario en un campo select.
+   * param o1 Primer objeto de tipo Pais a comparar
+   * param o2 Segundo objeto de tipo Pais a comparar
+   * returns True si los objetos son iguales, false en caso contrario
+   */
   compararPais(o1: Pais, o2:Pais): boolean{
     if(o1 === undefined && o2 === undefined){
       return true;
